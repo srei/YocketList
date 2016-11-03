131b1a8437b06debac7cc166d5cf4f3277af5467
srei <steferu@gmail.com>
2016-11-02 17:30:47 -0700

    grid style display for thumbnails

--- client/components/Queuelist.js
+++ client/components/Queuelist.js
@@ -4,15 +4,15 @@ import FlipMove from 'react-flip-move';
 
 const QueueList = ({ thumbnailClick, queues }) => {
   // Create Queue component for URLs. Format validation moved to server.
-  const validUrls = queues.map((queue) => <Queue thumbnailClick={thumbnailClick} key={queue.url} 
-  link={queue.url} score={queue.score} />);
-  
+  const validUrls = queues.map((queue) => <Queue thumbnailClick={thumbnailClick} key={queue.url}
+  link={queue.url} score={queue.score} />)
+
   return (
-    <div id="queueDiv">
+    <div className="queueDiv">
       <FlipMove easing="cubic-bezier(0, 0.7, 0.8, 0.1)">
         {validUrls}
       </FlipMove>
-    </div>  
+    </div>
   );
 };
 
